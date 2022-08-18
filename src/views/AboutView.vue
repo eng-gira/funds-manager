097c3328e9ea1b5020c4dd2f3604a5936625e2e3
A. Osama <Altimos@desktop.com>
2022-08-18 18:46:45 +0200

    Minor updates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,10 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <div class="about">
-    <h1>
-      This application was built by a Web Developer for experimental purposes. Use at your own risk. 
+    <h1> About this Web App </h1>
+    <h3>
+      This web app was built by a Web Developer for experimental purposes. Use at your own risk. 
       You can find the developer on this <a href="https://github.com/eng-gira" target="_blank">GitHub</a> link.
-    </h1>
+    </h3>
   </div>
 </template>
