ec890f8e216b1477a1b739641c1a5450f9cdf0d3
A. Osama <Altimos@desktop.com>
2022-12-27 04:34:52 +0200

    Minor design edit

--- src/views/DepositsHistoryView.vue
+++ src/views/DepositsHistoryView.vue
@@ -10,11 +10,11 @@
         <th class="p-2 uppercase font-bold text-gray-500 text-[10px]">Notes</th>
       </tr>
       <tr class="" v-for="deposit in deposits" :key="deposit.id">
-        <td class="p-2 font-bold text-[10px] lg:text-sm">{{ deposit.depositSource }}</td>
-        <td class="p-2 font-bold text-[10px] lg:text-sm">{{ deposit.depositedTo }}</td>
-        <td class="p-2 font-bold text-[8px] lg:text-[10px]">{{ deposit.createdOn }}</td>
-        <td class="p-2 font-bold text-[10px] lg:text-sm">{{ deposit.depositedAmount }}</td>
-        <td class="p-2 text-[10px]">{{ deposit.notes }}</td>
+        <td class="p-2 text-center font-bold text-[10px] lg:text-sm">{{ deposit.depositSource }}</td>
+        <td class="p-2 text-center font-bold text-[10px] lg:text-sm">{{ deposit.depositedTo }}</td>
+        <td class="text-center font-bold text-[7px] lg:text-[10px]">{{ deposit.createdOn }}</td>
+        <td class="p-2 text-center font-bold text-[10px] lg:text-sm">{{ deposit.depositedAmount }}</td>
+        <td class="p-2 text-center text-[10px]">{{ deposit.notes }}</td>
       </tr>
     </table>
   </div>
